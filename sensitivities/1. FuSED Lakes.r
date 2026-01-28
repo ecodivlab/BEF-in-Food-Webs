20cd70f66c9a45bcbf56e5f52525a03e528ac860
Benoit <benoit@Benoits-MacBook-Air.local>
2026-01-28 12:08:25 +0100

    preparing for sensitiviry analysis

---
+++ sensitivities/1. FuSED Lakes.r
@@ -0,0 +1,194 @@
+#=========================================================================================================================
+#     
+# Code for Barnes et al. 'Food Web Complexity Underlies the Relationship Between Biodiversity and Ecosystem Functioning'
+# Calculation of energy fluxes in 48 lake food webs
+#
+#=========================================================================================================================
+
+# This code calculates energy fluxes based on organism metabolic rates (estimated from body mass and temperature), 
+# trophic assimilation efficiency (based on resource type and temperature), and food web structure, following methods 
+# described in Gauzens et al. (2019) DOI: 10.1111/2041-210X.13109 
+# Code was developed on R version 4.5.0
+
+rm(list=ls())
+
+## To run this code, a local working directory must be set where all accompanying data and source code are lodged ##
+#setwd()
+
+source("Food_web_functions.r")
+library(fluxweb); library(cheddar); library(igraph); library(RColorBrewer); 
+library(colorspace); library(sp); library(dplyr); library(tibble)
+
+options(stringsAsFactors=FALSE)
+
+#### Adirondack and Maggiore lakes ####
+
+boltz <- 0.00008617343
+T0 <- 273.15 + 20   
+perday <- 60*60*24
+meta <- read.csv("Lakes/Lakes_metadata.csv")
+lakes <- unique(meta$FW_name)
+
+#### Omnivory function ####
+Omnivory.species = function(i, fw, TL){
+  # computes omnivory of species i
+  # TL: vector of all species' TLs
+  if (TL[i] == 1) {
+    omn = 0
+    return(omn)
+  }
+  prey = fw[,i] != 0
+  omn = 1/sum(fw[,i]) * sum(fw[prey,i] * (TL[prey] - (TL[i] - 1))^2)
+  return(omn)
+}
+
+
+#### Calculate food web properties and energy fluxes ####
+for(i in 1:length(lakes)) {
+  
+  # imports
+    lake <- NULL; nodes <- NULL; matrix<-NULL
+    lake <- lakes[i]
+    nodes <- read.csv(paste0("Lakes/Lakes_spAttributes_",lake,".csv"))
+     
+    # corrections to match database
+    if (meta$study_ID[i]=='Adirondack lakes'){
+    nodes$taxonomy[nodes$taxonomy=="Salmo trutta"] <- "Salmo rutta" # should be trutta but match to incorrect edgelist
+    nodes$taxonomy[nodes$taxonomy=="Anabaena flos-aquae"] <- "Anabaena flos.aquae"
+    }
+      
+    matrix <- as.matrix(read.csv(paste0("Lakes/Lakes_matrix_",lake,".csv"), row.names = 1))
+
+  # check order
+    nodes <- nodes[order(nodes$taxonomy),]
+    matrix <- matrix[order(rownames(matrix)),order(colnames(matrix))]
+    
+  # remove taxa that do not have matches, and with 0 density
+    unmatched.rows <- NULL; fishy <- NULL; detr <- NULL
+    unmatched.rows <- which(is.na(nodes$Count.of.Lake_species)) # which are missing? Could not be matched with density from report
+    eggs <- which(grepl("fish eggs", nodes$taxonomy)) # but keep fish eggs
+    detr <- which(grepl("detritus", nodes$taxonomy))  # but keep detritus
+    nauplii <- which(grepl("nauplii", nodes$taxonomy)) 
+    unmatched.rows <- unmatched.rows[!(unmatched.rows %in% c(detr))] ## <--- add 'eggs' here to keep eggs
+
+    if(length(unmatched.rows)>0) {
+         nodes <- nodes[-unmatched.rows,]
+         matrix <- matrix[-unmatched.rows,-unmatched.rows]
+    }
+    
+  # temperature
+    temp.K <- meta$temperature_C[meta$FW_name==lake] + 273.15
+    temp.arr <- ((temp.K)-T0)/(boltz*(temp.K)*T0)
+    
+  # losses
+    nodes$losses <- 0
+    nodes$losses[nodes$metabolic.type=="invertebrate"] <- exp((0.71 * log(nodes$mass.mean.g.[nodes$metabolic.type=="invertebrate"]) + 17.17) - 0.69/(boltz*(temp.K))) * nodes$density.comb[nodes$metabolic.type=="invertebrate"]
+    nodes$losses[nodes$metabolic.type=="ectotherm vertebrate"] <- exp((0.71 * log(nodes$mass.mean.g.[nodes$metabolic.type=="ectotherm vertebrate"]) + 18.47) - 0.69/(boltz*(temp.K))) * nodes$density.comb[nodes$metabolic.type=="ectotherm vertebrate"]
+    
+        ## Losses for fish eggs?? - unknown density - currently these are omitted anyway
+        nodes$losses[is.na(nodes$losses)] <- 0
+  
+  # efficiencies
+    nodes$efficiencies[nodes$metabolic.type=="invertebrate" | nodes$metabolic.type=="ectotherm vertebrate"] <- exp(2.266)*exp(0.164*temp.arr) / (1 + exp(2.266)*exp(0.164*temp.arr)) #animal
+    nodes$efficiencies[nodes$metabolic.type=="primary producer"] <- exp(0.179)*exp(0.164*temp.arr) / (1 + exp(0.179)*exp(0.164*temp.arr)) # plant
+    nodes$efficiencies[nodes$metabolic.type=="detritus"] <- exp(-1.670)*exp(0.164*temp.arr) / (1 + exp(-1.670)*exp(0.164*temp.arr)) #detritus
+
+    # preferences (biomass matrix)
+    B.matrix <- matrix * nodes$biomass
+    # missing detritus node given sum of phytoplankton biomass for each predator (i.e. equal weight to green/brown origin)
+    missing.detritus <- nodes$metabolic.type=="detritus"
+    B.matrix[missing.detritus,] <- rep(colSums(B.matrix[nodes$metabolic.type=="primary producer",], na.rm=T) / sum(missing.detritus), each=sum(missing.detritus))
+    B.matrix[matrix!=0 & B.matrix==0] <- 1 # for when detritus is the only food source
+
+
+  ## Fluxweb analysis
+  diag(matrix) <- 0   # remove cannibalistic links
+  bin.matrix <- matrix
+  bin.matrix[bin.matrix>0] <- 1
+  igraph <- graph_from_adjacency_matrix(bin.matrix, mode="directed")
+  sim.mat = 1-(similarity(igraph, mode = 'in', method = 'jaccard'))
+  attributes <- nodes
+  attributes$losses[is.na(attributes$losses)] <- 0
+  attributes$TL <- TL(bin.matrix)[,1]
+  consumers <- (bin.matrix)[,1]
+  attributes$omnivory = sapply(1:nrow(bin.matrix), Omnivory.species, bin.matrix, attributes$TL, simplify = TRUE)
+  attributes$omnivory[attributes$TL == 1] <- NA
+  
+  animals <- attributes$metabolic.type == "invertebrate" | attributes$metabolic.type == "ectotherm vertebrate" 
+  plants <- attributes$metabolic.type == "primary producer"
+  detritus <- attributes$metabolic.type == "detritus" 
+  
+  prim.cons = attributes$TL == 2 # primary consumers
+  basals = attributes$TL == 1 # basal species
+  sec.cons = attributes$TL > 2  # secondary consumers
+  
+  attributes$biomass[attributes$TL==1] <- mean(attributes$biomass[attributes$TL>1], na.rm=T) ## Set basal resource biomass to equal mean consumer biomass
+
+  eat.basals = colSums(matrix[basals, , drop = FALSE])  > 0
+  eat.animals = colSums(matrix[animals,]) > 0
+  omnivores = eat.basals & eat.animals
+  primary.cons.and.omnivores = omnivores | prim.cons
+
+  ## Calculating fluxes 
+  # Total flux
+  if(meta$study_ID[i]=='Lake Maggiore'){
+  flux <- fluxing(mat=bin.matrix, biomasses=attributes$biomass, losses=attributes$losses, efficiencies=attributes$efficiencies,
+                  bioms.losses=F, bioms.prefs = T) * perday}
+  else if (meta$study_ID[i]=='Adirondack lakes'){
+  flux <- fluxing(mat=bin.matrix, biomasses=attributes$biomass, losses=attributes$losses, efficiencies=attributes$efficiencies,
+                  bioms.losses=F, bioms.prefs = T) * perday * 1000000 #*1000000 to convert from ml to cubic meters for Adirondack Lakes
+  }
+  # Per capita flux
+  PC.flux <- sweep(flux, 1, nodes$density.comb, FUN = "/")
+  
+  ## Food web stability 
+  # create vector that encodes for species types (animal, detritus or plant)
+  met.types = rep('animal', nrow(bin.matrix))
+  met.types[plants] = 'plant'
+  met.types[detritus] = 'detritus'
+  # compute stability (leading eigenvalue of the Jacobian)
+  stab = stability.value(val.mat = flux,
+                  biomasses = attributes$biomass,
+                  efficiencies = attributes$efficiencies,
+                  metabolic.types = met.types,
+                  ef.level = "prey"
+  )
+
+  ## save stability value:
+  meta$stability[i] = stab
+
+  ## Food web fluxes
+  meta$flux[i] <- sum(flux)
+  meta$second.consumption[i] <- sum(flux[animals])
+  meta$second.consumption[meta$second.consumption==0] <- NA
+  meta$prim.consumption[i] <- sum(flux[plants,], flux[detritus,])
+  # Per capita flux
+  meta$PC.predation[i] <- mean(PC.flux[animals], na.rm = TRUE)
+
+  ## Food web metrics
+  meta$S[i] <- Number.of.species(bin.matrix)
+  meta$density[i] <- sum(attributes$density.comb[animals], na.rm = TRUE)
+  meta$MaxTL[i] <- max(TL(bin.matrix))
+  meta$sim.sec.cons[i] = mean(sim.mat[sec.cons, sec.cons]) # trophic similarity for secondary consumers
+  meta$sim.prim.cons[i] = mean(sim.mat[primary.cons.and.omnivores, primary.cons.and.omnivores])
+  meta$sim.total[i] = mean(sim.mat[!basals, !basals]) # trophic similarity for whole food web
+  
+  meta$L[i] <- Number.of.links(bin.matrix)
+  meta$LD[i] <- Link.density(bin.matrix)
+  meta$C[i] <- Connectance(bin.matrix)
+  meta$omnivory[i] <- mean(attributes$omnivory, na.rm=T)
+  
+  # modularity
+  g = graph_from_adjacency_matrix(bin.matrix, mode = "undirected")
+  community = cluster_walktrap(g)
+  meta$modularity[i] = modularity(g, membership(community))
+}
+#view(meta[,-c(2:32, 34)])
+
+meta.Lakes <- meta[,-c(2:32, 34)]
+
+meta.Lakes <- filter(meta.Lakes, second.consumption > 0) #remove food webs with no secondary consumers (Brooke Trout and South Lake). 
+
+write.csv(meta.Lakes, file = 'meta.Lakes.csv', row.names = F)
+
+
---
+++ sensitivities/2. FuSED Marine.R
@@ -0,0 +1,248 @@
+#=========================================================================================================================
+#     
+# Code for Barnes et al. 'Food Web Complexity Underlies the Relationship Between Biodiversity and Ecosystem Functioning'
+# Calculation of energy fluxes in 131 marine food webs
+#
+#=========================================================================================================================
+
+# This code calculates energy fluxes based on organism metabolic rates (estimated from body mass and temperature), 
+# trophic assimilation efficiency (based on resource type and temperature), and food web structure, following methods 
+# described in Gauzens et al. (2019) DOI: 10.1111/2041-210X.13109
+# The code is run on two datasets: intertidal rockpools and Gulf of Riga Baltic Sea.  
+# Code was developed on R version 4.5.0
+
+rm(list=ls())
+
+## To run this code, a local working directory must be set where all accompanying data and source code are lodged ##
+#setwd()
+
+source("Food_web_functions.r")
+library(fluxweb); library(igraph); library(dplyr); library(cheddar); library(colorspace)
+options(stringsAsFactors=FALSE)
+
+
+#### Intertidal Rockpools ####
+
+boltz <- 0.00008617343
+T0 <- 273.15 + 20   
+meta.RP <- read.csv("Intertidalrockpools/Intertidalrockypools_metadata.csv")
+meta.RP$study_ID <- rep('Intertidal rockpools', nrow(meta.RP))
+perday <- 60*60*24
+web <- unique(meta.RP$FW_name)
+
+#### Omnivory function ####
+Omnivory.species = function(i, fw, TL){
+  # computes omnivory of species i
+  # TL: vector of all species' TLs
+  if (TL[i] == 1) {
+    omn = 0
+    return(omn)
+  }
+  prey = fw[,i] != 0
+  omn = 1/sum(fw[,i]) * sum(fw[prey,i] * (TL[prey] - (TL[i] - 1))^2)
+  return(omn)
+}
+
+
+for(i in 1:length(web)){
+  
+  temp.K = meta.RP$temperature_C[meta.RP$FW_name == web[i]] + 273.15
+  temp.arr = (temp.K-T0)/(boltz*(temp.K)*T0)
+  matrix <-as.matrix(read.csv(paste("Intertidalrockpools/Intertidalrockypools_matrix_",web[i],".csv",sep=""),sep=",",header=T))
+  row.names(matrix) <- colnames(matrix)
+  diag(matrix) <- 0   # remove cannibalistic links
+  igraph <- graph_from_adjacency_matrix(matrix, mode="directed")
+  sim.mat = 1-(similarity(igraph, mode = 'in', method = 'jaccard')) #calculates trophic Dissimilarity
+
+  attributes <- read.csv(paste("Intertidalrockpools/Intertidalrockypools_spAttributes_",web[i],".csv",sep=""))
+  attributes$bodymass <- attributes$bodymass*4 # *4 convert dry to fresh weight
+  attributes$abundance <- attributes$biomass/attributes$bodymass
+  attributes$metabolic_type[attributes$metabolic_type=="invertebrate"] <- "ectotherm invertebrate"
+  
+  animals <- attributes$metabolic_type != "primary producer"  
+  plants <- attributes$metabolic_type == "primary producer"
+
+  attributes$losses[attributes$metabolic_type=="ectotherm invertebrate"] <- exp((0.71 * log(attributes$bodymass[attributes$metabolic_type=="ectotherm invertebrate"]) + 17.17) 
+                                                                                - 0.69/(boltz*(temp.K))) * attributes$abundance[attributes$metabolic_type=="ectotherm invertebrate"]
+  attributes$losses[attributes$metabolic_type=="ectotherm vertebrate"] <- exp((0.71 * log(attributes$bodymass[attributes$metabolic_type=="ectotherm vertebrate"]) + 18.47) 
+                                                                                - 0.69/(boltz*(temp.K))) * attributes$abundance[attributes$metabolic_type=="ectotherm vertebrate"]
+  attributes$efficiencies[animals] <- exp(2.266)*exp(0.164*temp.arr) / (1 + exp(2.266)*exp(0.164*temp.arr))
+  attributes$efficiencies[plants] <- exp(0.179)*exp(0.164*temp.arr) / (1 + exp(0.179)*exp(0.164*temp.arr))
+  
+  attributes$losses[is.na(attributes$losses)] <- 0
+  attributes$TL <- TL(matrix)
+  attributes$omnivory = sapply(1:nrow(matrix), Omnivory.species, matrix, attributes$TL, simplify = TRUE)
+  attributes$omnivory[attributes$TL == 1] <- NA
+  
+  prim.cons = attributes$TL == 2 # primary consumers
+  basals = attributes$TL == 1 # basal species
+  sec.cons = attributes$TL > 2  # secondary consumers
+  
+  attributes$biomass[attributes$TL==1] <- mean(attributes$biomass[attributes$TL>1], na.rm=T) ## Set basal resource biomass to equal mean consumer biomass
+  #meta.RP$NAbiomass[i] <- sum(is.na(attributes$biomass))
+  ##Fill missing consumer biomass to equal biomass of 1 individual (only 15 out of 3837 nodes, or 0.39% of all nodes)
+  attributes$biomass[is.na(attributes$biomass)] <- attributes$bodymass[is.na(attributes$biomass)]
+
+  eat.plants = colSums(matrix[plants, , drop = FALSE])  > 0
+  eat.animals = colSums(matrix[animals,]) > 0
+  omnivores = eat.plants & eat.animals
+  
+  primary.cons.and.omnivores = omnivores | prim.cons
+  
+  ## Calculating fluxes 
+  # Total flux
+  flux <- fluxing(mat=matrix, biomasses=attributes$biomass, losses=attributes$losses, efficiencies=attributes$efficiencies, 
+                  bioms.losses=F, bioms.prefs = T) * perday
+  # Per capita flux
+  PC.flux <- sweep(flux, 1, attributes$abundance, FUN = "/")
+  
+  ## Food web stability 
+  # create vector that encodes for species types (animal, detritus or plant)
+  met.types = rep('animal', nrow(matrix))
+  met.types[plants] = 'plant'
+  # compute stability (leading eigenvalue of the Jacobian)
+  stab = stability.value(val.mat = flux,
+                         biomasses = attributes$biomass,
+                         efficiencies = attributes$efficiencies,
+                         metabolic.types = met.types,
+                         ef.level = "prey"
+  )
+  
+  ## save stability value:
+  meta.RP$stability[i] = stab
+
+  ## Food web fluxes
+  meta.RP$flux[i] <- sum(flux)
+  meta.RP$second.consumption[i] <- sum(flux[animals])
+  meta.RP$prim.consumption[i] <- sum(flux[plants,])  
+  # Per capita flux
+  meta.RP$PC.predation[i] <- mean(PC.flux[animals], na.rm = TRUE)
+
+  ## Food web metrics
+  meta.RP$S[i] <- Number.of.species(matrix)
+  meta.RP$density[i] <- sum(attributes$abundance[animals], na.rm = TRUE)
+  meta.RP$MaxTL[i] <- max(attributes$TL)
+  meta.RP$sim.sec.cons[i] = mean(sim.mat[sec.cons, sec.cons]) # trophic similarity for secondary consumers
+  meta.RP$sim.prim.cons[i] = mean(sim.mat[primary.cons.and.omnivores, primary.cons.and.omnivores])
+  meta.RP$sim.total[i] = mean(sim.mat[!basals, !basals]) # trophic similarity for whole food web
+  
+  meta.RP$L[i] <- Number.of.links(matrix)
+  meta.RP$LD[i] <- Link.density(matrix)
+  meta.RP$C[i] <- Connectance(matrix)
+  meta.RP$omnivory[i] <- mean(attributes$omnivory, na.rm=T)
+  
+  # modularity
+  g = graph_from_adjacency_matrix(matrix, mode = "undirected")
+  community = cluster_walktrap(g)
+  meta.RP$modularity[i] = modularity(g, membership(community))
+}
+
+
+
+
+#### Fluxweb analysis Baltic Sea ####
+
+meta.BS <- read.csv("baltic_sea/baltic_metadata.csv")
+meta.BS$study_ID <- rep('Baltic Sea', nrow(meta.BS))
+web <- unique(meta.BS$FW_name)
+
+for(i in 1:length(web)){
+  
+  temp.K = meta.BS$temperature_C[meta.BS$FW_name == web[i]] + 273.15
+  temp.arr = ((temp.K)-T0)/(boltz*(temp.K)*T0)
+  matrix <-as.matrix(read.csv(paste("baltic_sea/baltic_matrix_",web[i],".csv",sep=""),sep=",",header=T, row.names = 1))
+  diag(matrix) <- 0   # remove cannibalistic links
+  igraph <- graph_from_adjacency_matrix(matrix, mode="directed")
+  sim.mat = 1-(similarity(igraph, mode = 'in', method = 'jaccard'))
+  
+  attributes <- read.csv(paste("baltic_sea/baltic_spAttributes_",web[i],".csv",sep=""))
+  attributes <- attributes[attributes$species %in% row.names(matrix), ] 
+  attributes$bodymass <- attributes$bodymass
+  attributes$abundance <- attributes$biomass/attributes$bodymass
+  attributes$metabolic_type[attributes$metabolic_type=="invertebrate"] <- "ectotherm invertebrate"
+  
+  animals <- attributes$metabolic_type == "ectotherm invertebrate" | attributes$metabolic_type == "ectotherm vertebrate"
+  plants <- attributes$metabolic_type == "primary producer"
+  detritus <- attributes$metabolic_type == "detritus"
+  
+  attributes$losses[attributes$metabolic_type=="ectotherm invertebrate"] <- exp((0.71 * log(attributes$bodymass[attributes$metabolic_type=="ectotherm invertebrate"]) + 17.17) 
+                                                                                - 0.69/(boltz*(temp.K))) * attributes$abundance[attributes$metabolic_type=="ectotherm invertebrate"]
+  attributes$losses[attributes$metabolic_type=="ectotherm vertebrate"] <- exp((0.71 * log(attributes$bodymass[attributes$metabolic_type=="ectotherm vertebrate"]) + 18.47) 
+                                                                              - 0.69/(boltz*(temp.K))) * attributes$abundance[attributes$metabolic_type=="ectotherm vertebrate"]
+  
+  attributes$efficiencies[attributes$metabolic_type=="ectotherm invertebrate"|attributes$metabolic_type=="ectotherm vertebrate"] <- exp(2.266)*exp(0.164*temp.arr) / (1 + exp(2.266)*exp(0.164*temp.arr))
+  attributes$efficiencies[attributes$metabolic_type=="primary producer"] <- exp(0.179)*exp(0.164*temp.arr) / (1 + exp(0.179)*exp(0.164*temp.arr))
+  attributes$efficiencies[attributes$metabolic_type=="detritus"] <- exp(-1.670)*exp(0.164*temp.arr) / (1 + exp(-1.670)*exp(0.164*temp.arr))
+  
+  attributes$losses[is.na(attributes$losses)] <- 0
+  attributes$TL <- TL(matrix)
+  attributes$omnivory = sapply(1:nrow(matrix), Omnivory.species, matrix, attributes$TL, simplify = TRUE)
+  attributes$omnivory[attributes$TL == 1] <- NA
+  
+  prim.cons = attributes$TL == 2 # primary consumers
+  basals = attributes$TL == 1 # basal species
+  sec.cons = attributes$TL > 2  # secondary consumers
+  
+  attributes$biomass[attributes$TL==1] <- mean(attributes$biomass[attributes$TL>1], na.rm=T) ## Set basal resource biomass to equal mean consumer biomass
+   
+  eat.basals = colSums(matrix[basals, , drop = FALSE])  > 0
+  eat.animals = colSums(matrix[animals,]) > 0
+  omnivores = eat.basals & eat.animals
+  
+  primary.cons.and.omnivores = omnivores | prim.cons
+  
+  ## Calculating fluxes 
+  # Total flux
+  flux <- fluxing(mat=matrix, biomasses=attributes$biomass, losses=attributes$losses, efficiencies=attributes$efficiencies, 
+                  bioms.losses=F, bioms.prefs = T) * perday
+  # Per capita flux
+  PC.flux <- sweep(flux, 1, attributes$abundance, FUN = "/")
+  
+  ## Food web stability 
+  # create vector that encodes for species types (animal, detritus or plant)
+  met.types = rep('animal', nrow(matrix))
+  met.types[plants] = 'plant'
+  met.types[detritus] = 'detritus'
+  # compute stability (leading eigenvalue of the Jacobian)
+  stab = stability.value(val.mat = flux,
+                         biomasses = attributes$biomass,
+                         efficiencies = attributes$efficiencies,
+                         metabolic.types = met.types,
+                         ef.level = "prey"
+  )
+  
+  # save stability value:
+  meta.BS$stability[i] = stab
+
+  ## Food web fluxes  
+  meta.BS$flux[i] <- sum(flux)
+  meta.BS$second.consumption[i] <- sum(flux[eat.animals])
+  meta.BS$prim.consumption[i] <- sum(flux[basals,])  
+  # Per capita flux
+  meta.BS$PC.predation[i] <- mean(PC.flux[animals], na.rm = TRUE)
+
+  ## Food web metrics
+  meta.BS$S[i] <- Number.of.species(matrix)
+  meta.BS$density[i] <- sum(attributes$abundance[animals], na.rm = TRUE)
+  meta.BS$MaxTL[i] <- max(attributes$TL)
+  meta.BS$sim.sec.cons[i] = mean(sim.mat[sec.cons, sec.cons]) # trophic similarity for secondary consumers
+  meta.BS$sim.prim.cons[i] = mean(sim.mat[primary.cons.and.omnivores, primary.cons.and.omnivores])
+  meta.BS$sim.total[i] = mean(sim.mat[!basals, !basals]) # trophic similarity for whole food web
+  
+  meta.BS$L[i] <- Number.of.links(matrix)
+  meta.BS$LD[i] <- Link.density(matrix)
+  meta.BS$C[i] <- Connectance(matrix)
+  meta.BS$omnivory[i] <- mean(attributes$omnivory, na.rm=T)
+  
+  # modularity
+  g = graph_from_adjacency_matrix(matrix, mode = "undirected")
+  community = cluster_walktrap(g)
+  meta.BS$modularity[i] = modularity(g, membership(community))
+}
+
+
+commcols <- intersect(names(meta.RP), names(meta.BS))
+meta.Marine <- bind_rows(select(meta.RP, all_of(commcols)),
+                          select(meta.BS, all_of(commcols)))
+
+write.csv(meta.Marine, file = 'meta.Marine.csv', row.names = F)
